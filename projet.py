fdff4bac77ee1877fdd0b5bc4f637f330f4bd2f2
lisabdj <lisa.bdj.95@gmail.com>
2021-09-05 21:47:08 +0200

    Modification de la fonction d'initialisation de matrice

--- projet.py
+++ projet.py
@@ -5,34 +5,28 @@ import numpy as np
 def traduction(proteine):
 	aa_hydrophobe=["A", "I", "L", "M", "F", "W", "V", "Y"]
 	proteine_traduite=[]
-
 	for i in proteine:
-		#&print(i)
 		if (i in aa_hydrophobe):
-				
 			proteine_traduite.append("H")
-				#h=0
-				#print(i, "est polaire")
 		else:			
 			proteine_traduite.append("P")
-				#print(i, "est hydrophobe")
-	#chaine_traduite="".join(proteine_traduite)
 	return(proteine_traduite)
 
 
-def matrice_bidimensionnelle(proteine_traduite):
-	mat=np.zeros((len(proteine_traduite)+4,len(proteine_traduite)+4))
+def init_matrice(proteine_traduite):
+	mat=np.empty((len(proteine_traduite)+4, len(proteine_traduite)+4), dtype= str)
+	mat.fill("-")
+
 	for i in range(2, len(mat)):
-		for aa in proteine_traduite:
-			mat[i, 2+len(proteine_traduite)/2]=aa
+		for j in range(len(proteine_traduite)):
+			mat[2+(len(proteine_traduite)//2), j+2]=proteine_traduite[j]
 			
 	return(mat)
 
 
-
 prot_HP=(traduction("AILMFWVYGA"))
 print(prot_HP)
-print(matrice_bidimensionnelle(prot_HP))
+print(init_matrice(prot_HP))
 
 
 		
